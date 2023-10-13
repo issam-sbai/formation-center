d679d19347dd3a8b4cfcfc92c5ef1a87cee020a0
issam-sbai <130942646+issam-sbai@users.noreply.github.com>
2023-10-13 12:12:00 +0100

    first

---
+++ src/main/java/com/codingtech/formationcenter/FormationCenterApplication.java
@@ -0,0 +1,13 @@
+package com.codingtech.formationcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormationCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormationCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codingtech/formationcenter/FormationCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingtech.formationcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormationCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
